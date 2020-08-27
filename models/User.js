99600479d8e2aad70b4c1388cacea442a966b369
Darius Matthews <DariustheDeveloper@gmail.com>
2020-08-27 17:33:54 -0500

    started user model

---
+++ models/User.js
@@ -0,0 +1,18 @@
+// init dependencies
+const mongoose = require('mongoose');
+
+// user schema
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  }
+});
+
+export const User = mongoose.model('user', userSchema);
