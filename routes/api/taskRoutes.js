de9292c0cec03c3d977c38884c0b26543c2f506e
Darius Matthews <DariustheDeveloper@gmail.com>
2020-09-01 19:39:16 -0500

    added all task routes

--- routes/api/taskRoutes.js
+++ routes/api/taskRoutes.js
@@ -4,26 +4,37 @@ const Task = require('../../models/Task');
 
 // @api   GET
 // @desc  get all task
-router.get('/', () => {
-
+router.get('/', (req, res) => {
+  Task.find()
+    .then(task => res.json(task))
+    .catch(err => { if (err) throw err });
 });
 
 // @api   POST
 // @desc  create task
-router.post('/', () => {
+router.post('/', (req, res) => {
+  const newTask = new Task({
+    task: req.body.task,
+    createdAt: new Date.toString()
+  });
 
+  newTask.save()
+    .then(task => res.json(task))
+    .catch(err => { if (err) throw err })
 });
 
 // @api   PATCH
 // @desc  update task/mark task as completed
-router.patch('/:id', () => {
-
+router.patch('/:id', (req, res) => {
+  Task.findByIdAndUpdate(req.params.id, {
+    completedAt: new Date.toString()
+  }, err => { if (err) throw err });
 });
 
 // @api   DELETE
 // @desc  delete a task
-router.delete('/:id', () => {
-
+router.delete('/:id', (req, res) => {
+  Task.findByIdAndDelete(req.params.id, err => { if (err) throw err });
 });
 
 
